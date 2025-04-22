8b74ad776acb69e1f50a2f676383667b98da7b4a
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 16:17:51 +0200

    02 done

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,20 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = parseInt(prompt('Podaj liczbę do mnożenia'));
 
-
+for(let i=1; i<10; i++){
+    console.log(`${x} x ${i} = `, x * i);
+}
 
 /* rozwiązanie z pętlą while  */
+
+const y = parseInt(prompt('Podaj liczbę do potęgowania'))
+const a = parseInt(prompt('Do jakiej potęgi?'));
+
+let j = 0
+let sum = 0
+while(j < a){
+    sum = y * y
+    j++
+}
+console.log(`${y} do potęgi ${a} =`, sum);
