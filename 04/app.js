b1455fe6018909560b4d7adf428849124abd3e33
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 17:24:38 +0200

    04 done

--- 04/app.js
+++ 04/app.js
@@ -1,6 +1,13 @@
 
-const arr = createArray(1, 100)
+const arr = createArray(10, 200)
 console.log(arr);
+
+const largest = getLargest(arr)
+console.log(largest);
+
+const avg = getAvg(largest)
+console.log(avg);
+
 
 function createArray(min, max){
     const array = []
@@ -9,4 +16,18 @@ function createArray(min, max){
         array.push(randomNumber)
     }
     return array
+}
+
+function getLargest(array){
+    const sortedArray = array.sort(function(a, b){
+        return b - a
+    })
+    return sortedArray.slice(0, 10)
+}
+
+function getAvg(array){
+    const avg = array.reduce(function(acc, num){
+        return acc + num
+    }, 0)
+    return avg / array.length
 }
