695ad0364a08091097772049fc3274cf18c2f1c4
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 17:33:34 +0200

    05 started with creating Student constructor

---
+++ 05/app.js
@@ -0,0 +1,9 @@
+function Student(initFirstName, initLastName){
+    this.firstName = initFirstName;
+    this.lastName = initLastName
+    this.grades = {}
+}
+
+Student.prototype.addGrade = function(subject, grade){
+    
+}
