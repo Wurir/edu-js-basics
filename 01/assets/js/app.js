78aa48809c08b6a8c83a27806bc4683d6813da29
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 15:38:27 +0200

    01 done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,26 @@
 const a = '4.2';
 const b = 9;
 
-console.log(a, b);
+console.log(typeof a, typeof b);
+
+const add = parseFloat(a) + b
+console.log(add);
+
+const sub = b - a
+console.log(sub);
+
+const multi = (parseFloat(a) * b).toFixed(2)
+console.log(multi);
+
+const division = (parseFloat(a) / b).toFixed(2)
+console.log(division);
+
+const arr = [add, sub, multi, division]
+
+for(const num of arr){
+    if(num > 20){
+        console.log(num + ' jest wiekszy od 20');
+    } else {
+        console.log(num + ' jest mniejszy od 20');
+    }
+}
